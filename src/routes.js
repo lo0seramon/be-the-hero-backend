bee5863e1651d7d3c9b7741de89f2d6683de98ae
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-03-28 10:00:42 -0300

    Adicionando funcionalidade de login

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 const express =  require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 //ONG ROUTER
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+//List incidents by specif ongs
+routes.get('/profile', ProfileController.index);
 
 //INCIDENT ROUTER
 routes.get('/incidents', IncidentController.index);
